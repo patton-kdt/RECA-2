bf5cb2273379694a6d07352de4906719cc65df85
patton-kdt <101805801+patton-kdt@users.noreply.github.com>
2023-07-03 16:11:26 +0900

    Update 05-Add_dns_record.ps1

--- 05-Add_dns_record.ps1
+++ 05-Add_dns_record.ps1
@@ -19,3 +19,6 @@ Add-DnsServerResourceRecordPtr -Name "12" -ZoneName "10.10.10.in-addr.arpa" -Ptr
 
 Add-DnsServerResourceRecordA -Name "sa-esxi-03" -ZoneName "vclass.local" -IPv4Address "10.10.10.13"
 Add-DnsServerResourceRecordPtr -Name "13" -ZoneName "10.10.10.in-addr.arpa" -PtrDomainName "sa-esxi-03.vclass.local"
+
+Add-DnsServerResourceRecordA -Name "sa-esxi-04" -ZoneName "vclass.local" -IPv4Address "10.10.10.14"
+Add-DnsServerResourceRecordPtr -Name "14" -ZoneName "10.10.10.in-addr.arpa" -PtrDomainName "sa-esxi-04.vclass.local"
